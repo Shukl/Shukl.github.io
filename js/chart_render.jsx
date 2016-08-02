4f7ae87521b93234412db32dea09a2903ac72c64
Nitin Shukla <nitin.pr.shukla@outlook.com>
2016-08-02 16:41:54 +0530

    layout fix

--- js/chart_render.jsx
+++ js/chart_render.jsx
@@ -127,11 +127,11 @@ var MyApp = React.createClass({
                 <h1 className="main-title">Good Riddance Refuse Analysis for 2015</h1>
                 <div id="interactive-dashbaord"></div>
                 <div className="chart-row">
-                    <div id="monthly-weight" className="inline-chart">
+                    <div id="monthly-weight">
                         <react_fc.FusionCharts {...monthlyChartConfigs}/>
                     </div>
                 </div>
-                <div className="chart-row">
+                <div className="chart-row" className="inline-chart">
                     <div id="weekly-weight">
                         <react_fc.FusionCharts {...typeWeeklyConfigs}/>
                     </div>
