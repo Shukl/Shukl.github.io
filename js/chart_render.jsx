d4dcf2ad3183d3cf23b4b1692361ea11572d0a50
Nitin Shukla <nitin.pr.shukla@outlook.com>
2016-08-02 14:30:20 +0530

    style fixes

--- js/chart_render.jsx
+++ js/chart_render.jsx
@@ -139,7 +139,7 @@ var MyApp = React.createClass({
 
         return (
             <div>
-                <h1 className="main-title">Acme Inc. Revenue Analysis for 2015</h1>
+                <h1 className="main-title">Good Riddance Refuse Analysis for 2015</h1>
                 <div id="interactive-dashbaord"></div>
                 <div className="chart-row">
                     <div id="country-revenue">
